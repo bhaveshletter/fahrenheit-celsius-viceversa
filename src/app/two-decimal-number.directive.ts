25cfb82ae151c8436335c915a8c1e358309dcfd7
bhaveshletter <bhaveshletter@gmail.com>
2020-08-09 18:28:30 +0530

    condition modify

--- src/app/two-decimal-number.directive.ts
+++ src/app/two-decimal-number.directive.ts
@@ -1,48 +1,44 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core'
 
 @Directive({
-  selector: '[appTwoDecimalNumber]',
+	selector: '[appTwoDecimalNumber]',
 })
 export class TwoDecimalNumberDirective {
-  allowKeys: string[] = [
-    '0',
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-    '.',
-    'Backspace',
-    'Tab',
-    'End',
-    'Home',
-    'ArrowLeft',
-    'ArrowRight',
-    'Del',
-    'Delete',
-  ];
+	allowKeys: string[] = [
+		'0',
+		'1',
+		'2',
+		'3',
+		'4',
+		'5',
+		'6',
+		'7',
+		'8',
+		'9',
+		'.',
+		'Backspace',
+		'Tab',
+		'End',
+		'Home',
+		'ArrowLeft',
+		'ArrowRight',
+		'Del',
+		'Delete',
+	]
 
-  constructor(private ele: ElementRef) {}
+	constructor(private ele: ElementRef) {}
 
-  @HostListener('keydown', ['$event']) onKeyDown(event: KeyboardEvent) {
-    let value: string = this.ele.nativeElement.value;
-    // Allowing only allowKeys
-    if (
-      !this.allowKeys.includes(event.key) ||
-      (value.includes('.') && event.key === '.')
-    ) {
-      event.preventDefault();
-    } else if (
-      // Not allowing for more than 1 decimal digit
-      !['Backspace', 'Delete', 'Del'].includes(event.key) &&
-      value.includes('.') &&
-      value.split('.')[1].length == 1
-    ) {
-      event.preventDefault();
-    }
-  }
+	@HostListener('keydown', ['$event']) onKeyDown(event: KeyboardEvent) {
+		let value: string = this.ele.nativeElement.value
+		// Allowing only allowKeys or tot allowing for more than 1 decimal digit
+		if (
+			!this.allowKeys.includes(event.key) ||
+			(value.includes('.') &&
+				(event.key === '.' ||
+					(!['Backspace', 'Delete', 'Del'].includes(event.key) &&
+						value.split('.')[1].length == 1)))
+		) {
+			event.preventDefault()
+		}
+	}
 }
