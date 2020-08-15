d73f896239726c94aa8214eecdfe7477e726503e
bhaveshletter <bhaveshletter@gmail.com>
2020-08-15 15:08:49 +0530

    corrected old app text

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require("express"),
-  app = express(),
-  port = 8080;
+const express = require('express'),
+	app = express(),
+	port = 8080
 
-app.use(express.static(__dirname + "/dist/chain-analysis"));
+app.use(express.static(__dirname + '/dist/fahrenheit-celsius-viceversa'))
 
-app.get("/", function (req, res) {
-  res.sendFile("index.html", { root: "dist/chain-analysis" });
-});
+app.get('/', function (req, res) {
+	res.sendFile('index.html', { root: 'dist/fahrenheit-celsius-viceversa' })
+})
 
 app.listen(process.env.PORT || port, () =>
-  console.log(`Running on port ${process.env.PORT || 8080}`)
-);
+	console.log(`Running on port ${process.env.PORT || 8080}`)
+)
